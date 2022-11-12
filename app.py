fa73faae55f1af8ee8f607b28e7c4447f3d1cddb
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-12 21:00:02 +0700

    Initial data dummy for testing endpoint in frontend

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def hello_world():
+    result = {
+        'id': 1,
+        'name': 'apple',
+        'description:': 'apple is technology product one of the best in the world!'
+
+    }
+    return jsonify(result)
 
 
 if __name__ == '__main__':
