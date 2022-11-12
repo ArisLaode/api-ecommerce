38e6407945f23c56d0e3e50585f1d5bd4a17c0b9
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-13 00:17:18 +0700

    Update varibale data dummy

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app, resource={
+    r"/*": {
+        "origins": "*"
+    }
+})
 
 
 @app.route('/')
@@ -8,7 +14,7 @@ def hello_world():
     result = {
         'id': 1,
         'name': 'apple',
-        'description:': 'apple is technology product one of the best in the world!'
+        'description': 'apple is technology product one of the best in the world!'
 
     }
     return jsonify(result)
