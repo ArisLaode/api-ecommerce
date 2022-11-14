dc9e9e07829fd65dd794c99e047024e1fdb102f9
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-15 03:05:56 +0700

    Update schema product by id

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -13,5 +13,8 @@ class VariantsSchema(ma.Schema):
         fields = ('id', 'product_id', 'name', 'size', 'color', 'images')
 
 
+product_schema_by_id = ProductSchema()
 product_schema = ProductSchema(many=True)
+
+variants_schema_by_id = VariantsSchema()
 variants_schema = VariantsSchema(many=True)
