912c4fb3da10bbd17996757ebaad60ed33b157b3
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-15 06:41:27 +0700

    new migration

--- migrations/versions/df0fcd99395b_.py
+++ migrations/versions/df0fcd99395b_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 199b7c56f8a3
+Revision ID: df0fcd99395b
 Revises: 
-Create Date: 2022-11-15 01:13:22.095340
+Create Date: 2022-11-15 04:51:19.388256
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '199b7c56f8a3'
+revision = 'df0fcd99395b'
 down_revision = None
 branch_labels = None
 depends_on = None
@@ -30,9 +30,9 @@ def upgrade():
     )
     op.create_table('variants',
     sa.Column('id', sa.Integer(), nullable=False),
-    sa.Column('product_id', sa.Integer(), nullable=True),
+    sa.Column('product_id', sa.Integer(), nullable=False),
     sa.Column('name', sa.String(length=100), nullable=False),
-    sa.Column('size', sa.Integer(), nullable=False),
+    sa.Column('size', sa.String(length=20), nullable=False),
     sa.Column('color', sa.String(length=100), nullable=False),
     sa.Column('images', sa.String(length=255), nullable=False),
     sa.Column('created_at', sa.DateTime(), nullable=False),
