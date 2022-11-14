41d0bb31215f5cd518b7abed8f77085eff2a84d3
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-15 06:40:36 +0700

    update model product

--- app/models/product.py
+++ app/models/product.py
@@ -15,18 +15,3 @@ class Products(db.Model):
 
     def __repr__(self):
         return f"product : {self.name}"
-
-
-class Variants(db.Model):
-    __tablename__ = 'variants'
-    id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
-    name = db.Column(db.String(100), nullable=False)
-    size = db.Column(db.Integer, nullable=False)
-    color = db.Column(db.String(100), nullable=False)
-    images = db.Column(db.String(255), nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f"variant : {self.name}"
