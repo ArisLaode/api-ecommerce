f35a0cf983365ddcfbd3e47d313c3ff6563ee29e
laodearissaputra <aris.saputra@bithealth.co.id>
2022-11-15 01:41:42 +0700

    Add table name & add table variants

--- app/models/product.py
+++ app/models/product.py
@@ -4,11 +4,29 @@ from extensions import db
 
 
 class Products(db.Model):
+    __tablename__ = 'products'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(70), nullable=False)
-    description = db.Column(db.Text(), nullable=False)
-    images = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text(), nullable=True)
+    images = db.Column(db.String(255), nullable=False)
     logo_id = db.Column(db.String(100), nullable=False)
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"product : {self.name}"
+
+
+class Variants(db.Model):
+    __tablename__ = 'variants'
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    name = db.Column(db.String(100), nullable=False)
+    size = db.Column(db.Integer, nullable=False)
+    color = db.Column(db.String(100), nullable=False)
+    images = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"variant : {self.name}"
